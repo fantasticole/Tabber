10739cf251a8c9f8b47158bdc8e140f02c92adc0
FantastiCole <coleamurphy@gmail.com>
2015-04-21 13:06:55 -0400

    Create date object inside of 'Opened' and 'Closed' objects.
    Times for opens and closes get added to the array value for the date key. If no key exists for that date, the function creates the key.

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
 var tabcount = 0;
 var tabsclosed = 0;
 var tracker = {
-	'Opened': [],
-	'Closed': []
+	'Opened': {},
+	'Closed': {}
 };
 
+// {"Opened":{"4/21/2015":["1:05:35 PM","1:05:40 PM","1:05:49 PM","1:06:02 PM"]},"Closed":{"4/21/2015":["1:06:07 PM","1:06:08 PM","1:06:09 PM","1:06:09 PM","1:06:10 PM"]}}
+
+// var tracker = {
+// 	'Opened': [],
+// 	'Closed': []
+// };
+
 // {"Opened":[["4/21/2015","11:26:44 AM"],["4/21/2015","11:26:46 AM"],["4/21/2015","11:26:53 AM"],["4/21/2015","11:26:55 AM"],["4/21/2015","11:26:57 AM"]],"Closed":[["4/21/2015","11:27:06 AM"],["4/21/2015","11:27:07 AM"],["4/21/2015","11:27:07 AM"],["4/21/2015","11:27:08 AM"],["4/21/2015","11:27:08 AM"]]}
-
-// ancestry.forEach(function(person) {
-//   byName[person.name] = person;
-// });
-
-// var tracker = [];
-
 
 
 function setStorage(key, val) {
@@ -30,10 +30,13 @@ chrome.tabs.onCreated.addListener(function(tab){
 	setStorage('date', date);
 	var time = data.slice(data.indexOf(' ') + 1);
 	setStorage('tabtime', time);
-	tracker['Opened'].push([date, time]);
-	// tracker.push([date, time]);
+	if (tracker['Opened'][date] !== undefined){
+		tracker['Opened'][date].push(time);
+	}
+	else {
+		tracker['Opened'][date] = [time];
+	};
 	setStorage('tracker', tracker);
-	console.log(tracker);
 });
 
 chrome.tabs.onRemoved.addListener(function(tab){
@@ -44,6 +47,11 @@ chrome.tabs.onRemoved.addListener(function(tab){
 	setStorage('cDate', date);
 	var time = data.slice(data.indexOf(' ') + 1);
 	setStorage('cTabtime', time);
-	tracker['Closed'].push([date, time]);
+	if (tracker['Closed'][date] !== undefined){
+		tracker['Closed'][date].push(time);
+	}
+	else {
+		tracker['Closed'][date] = [time];
+	};
 	setStorage('tracker', tracker);
 });
